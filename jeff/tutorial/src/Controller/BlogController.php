ef6512db32824e10301a69df3f8519cf1127dea2
jeffresh <manuel.pallanunez@alum.uca.es>
2021-04-24 21:51:17 +0200

    :sparkles: add url generators in blog controller

--- jeff/tutorial/src/Controller/BlogController.php
+++ jeff/tutorial/src/Controller/BlogController.php
@@ -5,6 +5,7 @@
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class BlogController extends AbstractController {
     
@@ -12,7 +13,22 @@ class BlogController extends AbstractController {
      * @Route("/blog", name="blog_list", requirements={"page"="\d+"}, priority=2)
      */
      public function list(int $page = 1): Response{
-         // ...
+         // gnerate a URL with no route arguments
+         $signUpRape = $this->generateUrl('sign_up');
+
+         // generate a URL with route arguments
+         $userProfilePage = $this->generateUrl('user_profile', ['username' => $user->getUsername(),
+         ]);
+
+         // generated URLs are "aboslute paths" by defaults. Pass athrid optional 
+         // argument to generate different URLs (e.g an "absolute URL")
+
+         $signUpPage = $this->generateUrl("sign_up", [], UrlGeneratorInterface::ABSOLUTE_URL);
+
+         // when a route is localized, Symfony uses by dfeault the current request locale
+         // pass a different '_Locale' value if you want ot set the Locale explicitly
+
+         $signUpPageInDutch = $this->generateUrl('sign_up', ['_locale' => 'nl']);
      }
 
      /**

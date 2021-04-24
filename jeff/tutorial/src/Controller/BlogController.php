d73f6574437221aa89acf92f86dc98a0d94e5c79
jeffresh <manuel.pallanunez@alum.uca.es>
2021-04-24 17:37:56 +0200

    :sparkles: add blog list route

---
+++ jeff/tutorial/src/Controller/BlogController.php
@@ -0,0 +1,16 @@
+<?php
+
+    namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation;
+
+// /**
+//  * @Route("/blog", name="blog_list")
+//  */
+class BlogController extends AbstractController {
+
+     public function list(){
+         // ...
+     }
+}

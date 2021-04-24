717841ad805a8be81b7a08a6fc747a3735364c53
jeffresh <manuel.pallanunez@alum.uca.es>
2021-04-24 15:28:03 +0200

    :sparkles: update lucky controller with twig template

--- jeff/tutorial/src/Controller/LuckyController.php
+++ jeff/tutorial/src/Controller/LuckyController.php
@@ -1,14 +1,15 @@
 <?php 
     namespace App\Controller;
 
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation;
 
-    class LuckyController {
+    class LuckyController extends AbstractController {
         public function number(): Response {
             $number = random_int(0, 100);
-
-            return new Response(
-                '<html><body>Lucky number: '.$number.'</body></html>'
-            );
+            return $this->render('lucky/number.html.twig', [
+                'number' => $number,
+            ]);
         }
     }

6c40ffd3b1308b8b54b73705a79730f5710c58db
jeffresh <manuel.pallanunez@alum.uca.es>
2021-04-24 18:11:09 +0200

    :sparkles: add blog controller routes annotations, requirements and default value

--- jeff/tutorial/src/Controller/BlogController.php
+++ jeff/tutorial/src/Controller/BlogController.php
@@ -3,14 +3,23 @@
     namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-// /**
-//  * @Route("/blog", name="blog_list")
-//  */
 class BlogController extends AbstractController {
-
-     public function list(){
+    
+    /**
+     * @Route("/blog", name="blog_list", requirements={"page"="\d+"})
+     */
+     public function list(int $page = 1): Response{
          // ...
      }
+
+     /**
+      *  @Route("/blog/{slug}", name="blog_show")
+      */
+
+      public function show(string $slug): Response {
+
+      }
 }

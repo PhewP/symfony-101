f27f54df7182b10f1c86060404b64ce89922f9fb
jeffresh <manuel.pallanunez@alum.uca.es>
2021-04-28 12:10:06 +0200

    :sparkles: add pagination to articulos index

--- jeff/tutorial/src/Controller/ArticuloController.php
+++ jeff/tutorial/src/Controller/ArticuloController.php
@@ -10,6 +10,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Knp\Component\Pager\PaginatorInterface;
+
 /**
  * @Route("/articulo")
  */
@@ -18,10 +20,18 @@ class ArticuloController extends AbstractController
     /**
      * @Route("/", name="articulo_index", methods={"GET"})
      */
-    public function index(ArticuloRepository $articuloRepository): Response
+    public function index(PaginatorInterface $paginator, ArticuloRepository $articuloRepository, Request $request): Response
     {
+        $articulosQuery = $articuloRepository->searchAllArticles();
+
+        $pagination = $paginator->paginate(
+            $articulosQuery, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            3 /*limit per page*/
+        );
+    
         return $this->render('articulo/index.html.twig', [
-            'articulos' => $articuloRepository->findAll(),
+            'pagination' => $pagination
         ]);
     }
 
